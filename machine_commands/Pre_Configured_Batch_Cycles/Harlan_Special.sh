53a2716cd6ba83f9608be1907841d2ca31471f24
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-02-22 15:30:17 -0800

    Update Harlan_Special.sh

--- machine_commands/Pre_Configured_Batch_Cycles/Harlan_Special.sh
+++ machine_commands/Pre_Configured_Batch_Cycles/Harlan_Special.sh
@@ -1,6 +1,6 @@
 ---
 name: "Harlan Special"
-about: "Batch command for: Warmup Up Cycle, 3x Bacterial Reduction Cycle, then 3x Dry Flower, Pro+, 25-40lb"
+about: "Batch command for: Warmup Up Cycle, 3x Bacterial Reduction Cycle + 3x Dry Flower, Pro+, 25-40lb"
 labels: batch-cycle
 ---
 
