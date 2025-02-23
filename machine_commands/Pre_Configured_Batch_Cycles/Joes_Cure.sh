1cd988958c37fd23f50ab049fee6ceeee2c48d84
corviato1 <44780076+corviato1@users.noreply.github.com>
2025-02-22 17:45:57 -0800

    Create Joes_Cure.sh

---
+++ machine_commands/Pre_Configured_Batch_Cycles/Joes_Cure.sh
@@ -0,0 +1,10 @@
+---
+name: "Joe's Cure"
+about: "Warmup Up Cycle + Air Circulation Cycle + 11x Time Wait 60 + Time Weight 30 + Warmup Up Cycle + Air Circulation Cycle + Time Weight 30 +11x Time Wait 60 + Warmup Up Cycle + Air Circulation Cycle + 11x Time Wait 60 + Time Weight 30 + Warmup Up Cycle + Air Circulation Cycle + Time Weight 30 +11x Time Wait 60 + Warmup Up Cycle + Air Circulation Cycle + 23x Time Wait 60 + Warmup Up Cycle + Air Circulation Cycle + Warmup Up Cycle + Air Circulation Cycle + 23x Time Wait 60 + Warmup Up Cycle + Air Circulation Cycle + Warmup Up Cycle + Air Circulation Cycle + 23x Time Wait 60 + Warmup Up Cycle + Air Circulation Cycle + Warmup Up Cycle + Air Circulation Cycle + 23x Time Wait 60 + Warmup Up Cycle + Air Circulation Cycle"
+labels: batch-cycle
+---
+
+
+
+
+
